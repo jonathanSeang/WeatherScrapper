5a57ad481609f3c19c60e9c27fd1443e5598df42
jonathanSeang <70104642+jonathanSeang@users.noreply.github.com>
2022-05-08 16:01:19 -0700

    plots period to temp

--- dataPrinters.py
+++ dataPrinters.py
@@ -1,6 +1,10 @@
-def printNumPeriodsPerWeather(forecast_items):
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import make_interp_spline
+
+def printNumPeriodsPerWeather(forecastItems):
     map = {}
-    for item in forecast_items:
+    for item in forecastItems:
         currDesc = item.find(class_="short-desc").get_text()
         map[currDesc] = map.get(currDesc, 0) + 1
 
@@ -10,3 +14,31 @@ def printNumPeriodsPerWeather(forecast_items):
     for item in itemsSorted: 
         print("{:<50} {:<10}".format(item, map[item]))
     print("{:<50} {:<10}".format("--------------------------------", "--------"))
+
+def plotPeriodTemp(forecastItems):
+    allPeriods = []
+    allTemps = []
+    for item in forecastItems:
+        period = item.find(class_="period-name").get_text()
+        temp = int(item.find(class_="temp").get_text()[-6:-3])
+        allPeriods.append(period)
+        allTemps.append(temp)
+
+    # make smooth
+    idx = range(len(allPeriods))
+    xnew = np.linspace(min(idx), max(idx), 300)
+
+    spl = make_interp_spline(idx, allTemps, k=3)
+    smooth = spl(xnew)
+
+    plt.rc('font', size=5)
+    plt.title("Period to temperature")
+    plt.xlabel("Period")
+    plt.ylabel("Temperature °F")
+
+    plt.plot(xnew, smooth)
+    plt.xticks(idx, allPeriods)
+    plt.savefig('periodTemp.png', bbox_inches='tight')
+    plt.show()
+    
+
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import dataRetriever
 import dataPrinters
+import matplotlib.pyplot as plt
 
 def main():
 
     forecastItems = dataRetriever.getForecastItems()
-    print(forecastItems)
-    #dataPrinters.printNumPeriodsPerWeather(forecastItems)
+    # dataPrinters.printNumPeriodsPerWeather(forecastItems)
+    dataPrinters.plotPeriodTemp(forecastItems)
 
 if __name__ == "__main__":
     main()
-
